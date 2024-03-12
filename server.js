dbe0534e87de0700fbf4e6ffe51a6eb3a3961f97
Harry Muriithi <hharryjjack@gmail.com>
2024-03-12 22:13:11 +0300

    Authenticate a user 2

--- server.js
+++ server.js
@@ -1,13 +1,14 @@
 // server.js
+
 import express from 'express';
-import routes from './routes';
+import router from './routes/index';
 
 const app = express();
+const PORT = process.env.PORT || 5000; // Use process.env.PORT if available, otherwise use port 5000
 
-const PORT = process.env.PORT || 5000;
-
-app.use('/', routes);
+app.use(express.json());
+app.use(router);
 
 app.listen(PORT, () => {
-  console.log(`Server running on port ${PORT}`);
+  console.log(`Server is running on port ${PORT}`);
 });
